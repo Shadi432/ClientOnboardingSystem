f0ef54dc7e337f337c15af123f8664483c3398ad
Mahdi <wad21ytu@uea.ac.uk>
2025-04-18 12:08:08 +0100

    Savepoint

--- frontend/src/pages/header.tsx
+++ frontend/src/pages/header.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router"
 
+// Used for the login form
+export async function action({request}: any){
+  const formData = await request.formData();
+  const username = formData.get("username");
+  const password = formData.get("password");
+
+  // Password needs to be hashed here etc...
+  console.log(username + " " + password);
+  return 10;
+}
+
 function App() {
   return (
     <>
-      {/* Text Box */}
-      {/* // Button 2 */}
-
         <h1 id="header">Client Onboarding System</h1>
         <Outlet />
     </>
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,9 +1,9 @@
 import { Form } from "react-router";
 
-const LOGIN_USER_URL =
-    import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_DEVELOPMENT_LOGIN_USER_URL
-    : import.meta.env.VITE_PRODUCTION_LOGIN_USER_URL;
+// const LOGIN_USER_URL =
+//     import.meta.env.MODE === "development"
+//     ? import.meta.env.VITE_DEVELOPMENT_LOGIN_USER_URL
+//     : import.meta.env.VITE_PRODUCTION_LOGIN_USER_URL;
 
 // const updateState = (value) => {
 //     console.log(value);
@@ -12,7 +12,7 @@ const LOGIN_USER_URL =
 function Login(){
     return(
         <>
-            <Form action={LOGIN_USER_URL} method="post">                
+            <Form action="/" method="post">                
                 <span className="inputTitle"> Login </span>
                 <input className="inputBox" name="username" type="text" placeholder="Enter Username"/>
                 <input className="inputBox" name="password" type="text" placeholder="Enter Password"/>
